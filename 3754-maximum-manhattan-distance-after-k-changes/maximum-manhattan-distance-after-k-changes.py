16e17ecc61575db46516c3cc90acccc6eabd5b14
reddyeswaranush <eswaranushreddy@gmail.com>
2025-06-20 11:56:10 +0530

    Time: 1269 ms (100.00%) | Memory: 22 MB (6.25%) - LeetSync

---
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        x, y = 0, 0
+        distances = []
+        for move in s:
+            if move == "N":
+                y += 1
+            elif move == "S":
+                y -= 1
+            elif move == "E":
+                x += 1
+            elif move == "W":
+                x -= 1
+            distances.append(abs(x) + abs(y))
+
+        if k == 0:
+            return max(distances)
+
+        max_dist = distances[1]
+        previous = distances[0]
+        added_boost = 0
+
+        for i in range(1, len(distances)):
+            if distances[i] < previous and k > 0:
+                added_boost += 2
+                k -= 1
+            previous = distances[i]
+            distances[i] += added_boost
+            max_dist = max(max_dist, distances[i])
+
+        return max_dist
