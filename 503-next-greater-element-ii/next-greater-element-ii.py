9044dd85d2543246a1e6e5c6af932dabc4d29ee1
REDDY ESWAR ANUSH <REDDY_23UCS173@ug.nita.ac.in>
2025-11-07 23:47:35 +0530

    Time: 19 ms (80.94%) | Memory: 19.8 MB (18.78%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        a=nums*2
-        ans=[]
         n=len(nums)
-        for i in range(n):
-            b=a[i]
-            found=False
-            for j in range(i+1,2*n):
-                if b<a[j]:
-                    ans.append(a[j])
-                    found=True
-                    break
-            if not found:
-                ans.append(-1)
+        ans=[-1]*n
+        stack=[]
+        for i in range(2*n):
+            b=nums[i%n]
+            while stack and nums[stack[-1]]<b:
+                ans[stack[-1]]=b
+                stack.pop()
+            if i<n:
+                stack.append(i)
         return ans
