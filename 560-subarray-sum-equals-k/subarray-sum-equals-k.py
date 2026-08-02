629e76433a7e3714491c658f1dce01f4a9c9f0be
Arise <eswaranushreddy@gmail.com>
2026-08-02 08:57:00 +0530

    Time: 37 ms (32.63%) | Memory: 21.9 MB (56.60%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        a={0:1}
+        b=0
+        ans=0
+        for i in nums:
+            b=b+i
+            if b-k in a:
+                ans+=a[b-k]
+            if b not in a:
+                a[b]=0
+            a[b]+=1
+        return ans
