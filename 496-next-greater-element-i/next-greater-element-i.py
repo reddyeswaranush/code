cb963da64eff553f931be92281e58565a325843a
Arise <eswaranushreddy@gmail.com>
2026-08-10 22:55:31 +0530

    Time: 123 ms (5.09%) | Memory: 19.4 MB (68.00%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -2,15 +2,11 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         x=[]
         for i in nums1:
-            a=False
-            b=False
-            for j in nums2:
-                if i==j:
-                    a=True
-                if a==True and j>i:
-                    x.append(j)
-                    b=True
-                    break
-            if b==False:
-                x.append(-1)
+            a=nums2[:]
+            maxx=-1
+            while a[-1]!=i:
+                b=a.pop()
+                if b>i:
+                    maxx=b
+            x.append(maxx)
         return x
